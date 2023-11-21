b1ca867150d60cf5e98873c05f59363a8d79ae1f
adamxu <1254420756xyd@gmail.com>
2023-11-21 09:45:39 +0000

    init

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,96 @@
-import altair as alt
-import numpy as np
-import pandas as pd
 import streamlit as st
+import PyPDF2
+import os
+from typing import List
 
-"""
-# Welcome to Streamlit!
+# Streamlit config
+st.set_page_config(layout='wide')
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+class InvalidCVError(BaseException):
+    pass
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+class InvalidJDError(BaseException):
+    pass
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+class CV:
+    def __init__(self, name: str):
+        segments = name.split(".")[0].split("-")
+        if len(segments) != 6:
+            raise InvalidCVError
+        self.meta = segments[:2]
+        self.jid = segments[3]
+        self.jname = segments[4]
+        self.cid = segments[5]
+        self.source = os.path.join("./app/static", name)
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+    def __str__(self) -> str:
+        return f"""
+        😊 CV: {self.cid}
+        Applyed Job: {self.jname}
+        Metadata: {self.meta}"""
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
+class JD:
+    def __init__(self, name: str):
+        segments = name.split(".")[0].split("_")
+        if len(segments) != 3:
+            raise InvalidJDError
+        self.idx = segments[0]
+        self.jname = segments[1]
+        self.jid = segments[2]
+        self.matched = []
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
+    def __str__(self) -> str:
+        return self.jname
 
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+def read_pdf(file_path):
+    st.write(file_path)
+    with open(file_path, "rb") as f:
+        pdf = PyPDF2.PdfReader(f)
+        num_pages = pdf.getNumPages()
+        text = ""
+        for page_num in range(num_pages):
+            page = pdf.getPage(page_num)
+            text += page.extractText()
+        return text
+
+def _init():
+    cvs = [CV(cv) for cv in os.listdir("CV")]
+    jds = [JD(jd) for jd in os.listdir("JD")]
+    for jd in jds:
+        for cv in cvs:
+            if cv.jid == jd.jid:
+                jd.matched.append(cv)
+    return jds
+
+def show_jd_overview(containers, jd):
+    for idx, cv in enumerate(jd.matched):
+        containers[idx % len(containers)].button(
+            label=str(cv), 
+            key=cv.cid,
+            on_click=show_cv_detail,
+            args=[cv.source],
+            use_container_width=True)
+
+def show_cv_detail(file):
+    # Need to store files on obs and give access to application
+    # st.title('PDF Viewer')
+    # st.write(read_pdf(file))
+    pass
+
+def main():
+    jds = _init()
+    # UI part
+    cols = st.columns(spec=2, gap="large")
+    with st.sidebar:
+        st.title("Job Descriptions")
+        # add_jd = st.file_uploader(label="Upload a Job Description")
+        for jd in jds:
+            st.button(label=jd.jname,
+                    on_click=show_jd_overview,
+                    args=[cols, jd],
+                    use_container_width=True)
+
+
+if __name__ == "__main__":
+    main()
+    
